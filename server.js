4c25084ac100331622196d769474ce50914e7391
Your Name <Vaaax@Jon-PC>
2022-03-10 22:39:06 -0600

    hot fix pt 2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
